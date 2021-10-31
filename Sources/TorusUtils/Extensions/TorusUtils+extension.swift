272e2b6280d96ac15b61b2eb0da148aa51ea6316
michaellee8 <ckmichael8@gmail.com>
2021-10-31 19:59:25 +0800

    Update TorusUtils+extension.swift

--- Sources/TorusUtils/Extensions/TorusUtils+extension.swift
+++ Sources/TorusUtils/Extensions/TorusUtils+extension.swift
@@ -111,7 +111,7 @@ extension TorusUtils {
             return promise
         }
         let request = try! self.makeUrlRequest(url: "https://metadata.tor.us/get")
-        let task = self.urlSession.uploadTask(.promise, with: request, from: encodedWrapped)
+        let task = self.urlSession.uploadTask(.promise, with: request, from: encodedUnwrapped)
         task.compactMap {
             try JSONSerialization.jsonObject(with: $0.data) as? [String: Any]
         }.done{ data in
