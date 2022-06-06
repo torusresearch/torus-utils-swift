b55c1b1898e935f345c3bb2d9653ac334f52da59
dhruv <dhruvjaiswal5@gmail.com>
2022-06-06 23:13:54 +0530

    minor change 6jun

--- Package.swift
+++ Package.swift
@@ -13,8 +13,7 @@ let package = Package(
             targets: ["TorusUtils"]),
     ],
     dependencies: [
-        
-        .package(name: "FetchNodeDetails", url: "https://github.com/torusresearch/fetch-node-details-swift.git",from: "2.6.0"),
+        .package(name:"FetchNodeDetails", url: "https://github.com/torusresearch/fetch-node-details-swift.git", .branch("web3-fix-6Jun")),
         .package(name:"PMKFoundation", url: "https://github.com/PromiseKit/Foundation.git", from: "3.4.0"),
         .package(name:"PromiseKit", url: "https://github.com/mxcl/PromiseKit.git", from: "6.0.0"),
         .package(name:"CryptorECC", url: "https://github.com/IBM-Swift/BlueECC.git", from: "1.2.4"),
