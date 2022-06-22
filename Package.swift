f4d9aa5683ab2f1de17909b8f47bae1dc91dfe4f
dhruv <dhruvjaiswal5@gmail.com>
2022-06-22 18:29:43 +0530

    web3 dependency removed

--- Package.swift
+++ Package.swift
@@ -18,16 +18,15 @@ let package = Package(
         .package(name:"PMKFoundation", url: "https://github.com/PromiseKit/Foundation.git", from: "3.4.0"),
         .package(name:"CryptoSwift", url: "https://github.com/krzyzanowskim/CryptoSwift.git",from: "1.5.1"),
         .package(name:"jwt-kit", url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
-        .package(name:"web3.swift", url: "https://github.com/argentlabs/web3.swift", from:"0.9.3"),
         .package(name: "TweetNacl", url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap.git", from: "1.0.0")
     ],
     targets: [
         .target(
             name: "TorusUtils",
-            dependencies: ["FetchNodeDetails", "CryptoSwift", "web3.swift", "PMKFoundation", "TweetNacl"]),
+            dependencies: ["FetchNodeDetails", "CryptoSwift", "PMKFoundation", "TweetNacl"]),
         .testTarget(
             name: "TorusUtilsTests",
-            dependencies: ["TorusUtils","CryptoSwift",.product(name: "JWTKit", package: "jwt-kit"), "FetchNodeDetails", "web3.swift", "PMKFoundation"]),
+            dependencies: ["TorusUtils","CryptoSwift",.product(name: "JWTKit", package: "jwt-kit"), "FetchNodeDetails", "PMKFoundation"]),
     ]
     ,swiftLanguageVersions: [.v5]
     
