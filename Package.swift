7377bb50454cac70ce24a29844e1c2abf777a508
dhruv <dhruvjaiswal5@gmail.com>
2022-07-06 17:04:53 +0530

    macOS support added for spm

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "TorusUtils",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),.macOS(.v10_12)
     ],
     products: [
         .library(
