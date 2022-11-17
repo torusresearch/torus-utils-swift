f463772b38d37bdc50e8ec117665d7bcc087e25b
dhruv <dhruv@tor.us>
2022-11-17 22:45:11 +0530

    CI fix attempt 2

--- Sources/TorusUtils/Extensions/URLSessionExtension.swift
+++ Sources/TorusUtils/Extensions/URLSessionExtension.swift
@@ -12,9 +12,9 @@ Fix for the issue
 */
 @available(iOS, deprecated: 15.0, message: "Use the built-in API instead")
 extension URLSession {
-    func data(from url: URL) async throws -> (Data, URLResponse) {
+    func data(from request: URLRequest) async throws -> (Data, URLResponse) {
         try await withCheckedThrowingContinuation { continuation in
-            let task = self.dataTask(with: url) { data, response, error in
+            let task = self.dataTask(with: request) { data, response, error in
                 guard let data = data, let response = response else {
                     let error = error ?? URLError(.badServerResponse)
                     return continuation.resume(throwing: error)
