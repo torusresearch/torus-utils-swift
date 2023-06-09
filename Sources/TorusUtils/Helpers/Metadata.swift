0c036b6dcfdae5f84c1a896c133b8b69d92fafd6
Pooja Chopra <pooja@docucomb.com>
2023-06-09 10:07:49 +0530

    remove BigUInt(0)

--- Sources/TorusUtils/Helpers/Metadata.swift
+++ Sources/TorusUtils/Helpers/Metadata.swift
@@ -6,7 +6,7 @@ func convertMetadataToNonce(params: [String: Any]?) -> BigUInt {
     guard let params = params, let message = params["message"] as? String else {
         return BigUInt(0)
     }
-    return BigUInt(message, radix: 16) ?? BigUInt(0)
+    return BigUInt(message, radix: 16)
 }
 
 func decryptNodeData(ciphertextHex: String, privateKey: String) -> String? {
