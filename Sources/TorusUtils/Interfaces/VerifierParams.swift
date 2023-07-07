4d06a8c42e1413ef85c6a5f802c7a95024f44236
ieow <cwlee0@gmail.com>
2023-07-07 18:29:47 +0800

    fix: make VerifierParams init public

--- Sources/TorusUtils/Interfaces/VerifierParams.swift
+++ Sources/TorusUtils/Interfaces/VerifierParams.swift
@@ -5,7 +5,7 @@ public struct VerifierParams {
     public let extended_verifier_id: String?
     public let additionalParams: [String: Codable]
     
-    init(verifier_id: String, extended_verifier_id: String? = nil, additionalParams: [String: Codable] = [:]) {
+    public init(verifier_id: String, extended_verifier_id: String? = nil, additionalParams: [String: Codable] = [:]) {
         self.verifier_id = verifier_id
         self.extended_verifier_id = extended_verifier_id
         self.additionalParams = additionalParams
