a98d8a1cfc195e10e4289b47235cb3d83d2d0a87
ieow <cwlee0@gmail.com>
2023-08-18 20:02:44 +0800

    fix: fnd urlSession require macos 10.15

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 let package = Package(
     name: "TorusUtils",
-    platforms: [.iOS(.v13), .macOS(.v10_13)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(
             name: "TorusUtils",
